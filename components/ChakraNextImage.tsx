83ef39083dbffa267bccbd06b00a28f4d9ee4626
Shreyas <shreyas314159@gmail.com>
2024-06-30 20:27:51 +0100

    new

--- components/ChakraNextImage.tsx
+++ components/ChakraNextImage.tsx
@@ -1,7 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { Box, BoxProps } from "@chakra-ui/react";
 import NextImage from "next/image";
 
-const ChakraNextImage = ({ src, alt, ...rest }) => {
+interface ChakraNextImageProps extends BoxProps {
+  src: string;
+  alt: string;
+}
+
+const ChakraNextImage: React.FC<ChakraNextImageProps> = ({
+  src,
+  alt,
+  ...rest
+}) => {
   return (
     <Box position="relative" {...rest}>
       <NextImage src={src} alt={alt} layout="fill" objectFit="cover" />
